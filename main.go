2273f46c9db572f39fa9e44dcd014b34775aa832
Praveen Ravichandran <me@pravinba9495.dev>
2022-01-09 16:47:40 -0500

    Show actual chain name instead of chain id in the console

--- main.go
+++ main.go
@@ -3,25 +3,30 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/pravinba9495/kryptonite/chains"
 	"github.com/pravinba9495/kryptonite/networks"
 )
 
 func main() {
 
-	address := flag.String("address", "0x0000000000000000000000000000000000000000", "Your wallet address")
+	address := flag.String("wallet", "0x0000000000000000000000000000000000000000", "Your wallet address")
 	chainID := flag.Int("chain", 1, "Your preferred chain")
 	stableCoin := flag.String("stable", "USDC", "Your preferred stable coin")
 	targetCoin := flag.String("target", "ETH", "Your high conviction coin")
 
 	flag.Parse()
 
-	log.Println("Your address: " + *address)
-	log.Println("Your preferred chain: " + fmt.Sprint(*chainID))
+	chain, err := chains.GetChainNameByID(*chainID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Your wallet address: " + *address)
+	log.Println("Your preferred chain: " + chain)
 	log.Println("Your preferred stable coin/token: " + *stableCoin)
 	log.Println("Your high conviction coin/token: " + *targetCoin)
 
