68a842d07d8a34fa5afe28d407e354061d01bc37
Praveen Ravichandran <me@pravinba9495.dev>
2022-01-09 13:55:54 -0500

    Add healthcheck api wrapper

---
+++ api/oneinch/health.go
@@ -0,0 +1,25 @@
+package oneinch
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+// GetHealthStatus checks the status of the 1inch router
+func GetHealthStatus(chainID int) error {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", ApiBaseUrl+"/"+ApiVersion+"/"+fmt.Sprint(chainID)+string(HealthcheckEndpoint), nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Accept", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return errors.New(resp.Status)
+	}
+	return nil
+}
