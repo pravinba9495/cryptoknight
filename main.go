40bb2ee093d05799749df4009ee7fe79c671723c
Praveen Ravichandran <me@pravinba9495.dev>
2022-01-18 15:41:11 -0500

    Move flag parser to separate file

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"errors"
-	"flag"
 	"fmt"
 	"log"
+	"math"
 	"math/big"
 	"os"
 	"strconv"
@@ -23,51 +23,16 @@ import (
 
 func main() {
 
-	// Parse input arguments
-	var address, privateKey, stableToken, targetToken, botToken, password, redisAddress, mode string
-	var chainID, profitPercent, stopLossPercent uint64
-	flag.StringVar(&address, "publicKey", "", "Your wallet public address")
-	flag.StringVar(&privateKey, "privateKey", "", "Your wallet private key")
-	flag.StringVar(&mode, "mode", "MANUAL", "Mode of operation. Allowed options: 'MANUAL' (requires you to authorize a swap via telgram bot), 'AUTO' (does not require any confirmation)")
-	flag.StringVar(&bot.ChatID, "chatId", "", "Your telegram chat id. You will receive this when you authorize yourself with the bot for the first time")
-	flag.StringVar(&stableToken, "stableToken", "USDC", "Stable token (ERC20) to use. Example: USDC, USDT, DAI")
-	flag.StringVar(&targetToken, "targetToken", "WETH", "Target ERC20 token to hold. Example: WETH, WMATIC, LINK.")
-	flag.StringVar(&botToken, "botToken", "", "Telegram bot token used to send and receive transaction confirmations")
-	flag.StringVar(&password, "password", "kryptonite", "Password to share with the bot to authorize yourself as the admin")
-	flag.StringVar(&redisAddress, "redisAddress", "", "Redis server host. Example: 192.168.1.100:6379")
-	flag.Uint64Var(&chainID, "chainId", 1, "Chain to use. Allowed options: 1 (Ethereum), 10 (Optimism), 56 (Binance Smart Chain), 137 (Polygon/Matic), 42161 (Arbitrum)")
-	flag.Uint64Var(&profitPercent, "profitPercent", 50, "Profit percent at which the bot will execute a sell order")
-	flag.Uint64Var(&stopLossPercent, "stopLossPercent", 25, "Loss percent at which the bot will execute a stop loss order")
-	flag.Parse()
-
-	if botToken == "" {
-		log.Fatalln("botToken is not provided")
+	if err := variables.ParseFlags(); err != nil {
+		log.Fatalln(err)
 	}
 
-	go bot.Run(botToken, password)
+	go bot.Run(variables.BotToken, variables.Password)
 	time.Sleep(2 * time.Second)
-
-	if privateKey == "" {
-		err := "privateKey is not provided"
-		Die(errors.New(err))
-	}
-
-	if redisAddress == "" {
-		err := "redis address is not provided"
-		Die(errors.New(err))
-	}
-
-	if (mode != "MANUAL") && (mode != "AUTO") {
-		err := "unsupported mode"
-		Die(errors.New(err))
-	}
-
-	// Set bot mode
-	variables.BotMode = mode
 
 	// Connect to redis
 	rdb := redis.NewClient(&redis.Options{
-		Addr: redisAddress,
+		Addr: variables.RedisAddress,
 	})
 
 	// Check redis ping
@@ -76,36 +41,23 @@ func main() {
 	}
 
 	// Initialize wallet
-	wallet, err := (&models.Wallet{}).New(address, privateKey, chainID)
+	wallet, err := (&models.Wallet{}).New(variables.Address, variables.PrivateKey, variables.ChainID)
 	if err != nil {
 		Die(err)
 	}
 
 	// Initialize swap router
-	router, err := (&models.Router{}).New(chainID)
+	router, err := (&models.Router{}).New(variables.ChainID)
 	if err != nil {
 		Die(err)
 	}
 
-	// Get coin list from CoinGecko
-	coins, err := coingecko.GetCoinsList()
+	targetCoinID, err := coingecko.GetCoinID(variables.TargetToken)
 	if err != nil {
 		Die(err)
 	}
 
-	targetCoinID := ""
-	for _, coin := range coins {
-		if coin.Symbol == strings.ToLower(targetToken) {
-			targetCoinID = coin.ID
-			break
-		}
-	}
-	if targetCoinID == "" {
-		err := "coin could not be found"
-		Die(errors.New(err))
-	}
-
-	stableTokenContractAddress, targetTokenContractAddress := helpers.GetTokenAddress(router, stableToken), helpers.GetTokenAddress(router, targetToken)
+	stableTokenContractAddress, targetTokenContractAddress := helpers.GetTokenAddress(router, variables.StableToken), helpers.GetTokenAddress(router, variables.TargetToken)
 	if stableTokenContractAddress == "" || targetTokenContractAddress == "" {
 		err := "token pair does not exist on this chain/network/router"
 		Die(errors.New(err))
@@ -139,8 +91,8 @@ func main() {
 					}
 
 					// Print token addresses and balances
-					log.Print(fmt.Sprintf("%s [%s] => %s (wei)", targetToken, targetTokenContractAddress, wallet.TargetCoinBalance.String()))
-					log.Print(fmt.Sprintf("%s [%s] => %s (wei)", stableToken, stableTokenContractAddress, wallet.StableCoinBalance.String()))
+					log.Print(fmt.Sprintf("%s [%s] => %s (wei)", variables.TargetToken, targetTokenContractAddress, wallet.TargetCoinBalance.String()))
+					log.Print(fmt.Sprintf("%s [%s] => %s (wei)", variables.StableToken, stableTokenContractAddress, wallet.StableCoinBalance.String()))
 
 					// Get current token price
 					currentTokenPrice, err := coingecko.GetCoinPrice(targetCoinID)
@@ -148,16 +100,15 @@ func main() {
 						bot.OutboundChannel <- err.Error()
 					} else {
 						log.Print(fmt.Sprintf("Current Status: %s", currentStatus))
-
 						variables.CurrentStatus = currentStatus
 						variables.Verdict = "Verdict => Nothing to do"
 						str := ""
 
 						// Check if current status is WAITING_TO_BUY/WAITING_TO_SELL/UNKNOWN
 						if currentStatus == "WAITING_TO_BUY" && wallet.StableCoinBalance.Cmp(big.NewInt(0)) > 0 {
-							v, err := rdb.HGet(context.TODO(), strings.ToUpper(stableToken)+"_"+strings.ToUpper(targetToken), "BuyLimit").Result()
+							v, err := rdb.HGet(context.TODO(), strings.ToUpper(variables.StableToken)+"_"+strings.ToUpper(variables.TargetToken), "BuyLimit").Result()
 							if err == redis.Nil {
-								_, err := rdb.HSet(context.TODO(), strings.ToUpper(stableToken)+"_"+strings.ToUpper(targetToken), "BuyLimit", 0).Result()
+								_, err := rdb.HSet(context.TODO(), strings.ToUpper(variables.StableToken)+"_"+strings.ToUpper(variables.TargetToken), "BuyLimit", 0).Result()
 								if err != nil {
 									Die(err)
 								}
@@ -170,41 +121,73 @@ func main() {
 									bot.OutboundChannel <- err.Error()
 								} else {
 									// If currentTokenPrice is a BUY
-									if technical.IsABuy(currentTokenPrice, buyLimit) {
-										// Print stats
-										str = fmt.Sprintf("Verdict => BUY\n\nCurrent Price: $%f\nBuy Limit: $%f\n\nCurrent Price has reached Buy Limit Price\n", currentTokenPrice, buyLimit)
-										if err := router.DoSwap(wallet, stableTokenContractAddress, wallet.StableCoinBalance, targetTokenContractAddress, variables.BotMode); err != nil {
-											if err.Error() == "REQUEST_EXPIRED_OR_DECLINED" {
-												err = errors.New("Request expired/declined")
+									quoteParams := &models.QuoteParamsDto{
+										FromTokenAddress: stableTokenContractAddress,
+										ToTokenAddress:   targetTokenContractAddress,
+										Amount:           wallet.StableCoinBalance.String(),
+									}
+									quoteResDto, err := router.GetQuote(router.ChainID, quoteParams)
+									if err != nil {
+										bot.OutboundChannel <- err.Error()
+									} else {
+
+										toTokenAmount := new(big.Float)
+										toTokenAmount.SetString(quoteResDto.ToTokenAmount)
+										s := new(big.Float).Quo(toTokenAmount, big.NewFloat(math.Pow10(int(quoteResDto.ToToken.Decimals))))
+
+										fromTokenAmount := new(big.Float)
+										fromTokenAmount.SetString(quoteResDto.FromTokenAmount)
+										m := new(big.Float).Quo(fromTokenAmount, big.NewFloat(math.Pow10(int(quoteResDto.FromToken.Decimals))))
+
+										tokenAmountFromSwap, err := strconv.ParseFloat(s.String(), 64)
+										if err != nil {
+											bot.OutboundChannel <- err.Error()
+										} else {
+											maxTokensReceivableAccordingToPrice, err := strconv.ParseFloat(m.String(), 64)
+											if err != nil {
 												bot.OutboundChannel <- err.Error()
 											} else {
-												Die(err)
-											}
-										} else {
-											_, err := rdb.HSet(context.TODO(), strings.ToUpper(stableToken)+"_"+strings.ToUpper(targetToken), "PreviousTokenPrice", currentTokenPrice).Result()
-											if err != nil {
-												Die(err)
-											} else {
-												_, err := rdb.HSet(context.TODO(), strings.ToUpper(stableToken)+"_"+strings.ToUpper(targetToken), "SellLimit", currentTokenPrice*float64(1+(profitPercent/100))).Result()
-												if err != nil {
-													Die(err)
-												} else {
-													_, err := rdb.HSet(context.TODO(), strings.ToUpper(stableToken)+"_"+strings.ToUpper(targetToken), "StopLimit", currentTokenPrice*float64(1-(stopLossPercent/100))).Result()
-													if err != nil {
-														Die(err)
+												tokenAmountFromPrice := maxTokensReceivableAccordingToPrice / currentTokenPrice
+												if technical.IsABuy(currentTokenPrice, buyLimit, tokenAmountFromSwap, tokenAmountFromPrice) {
+													// Print stats
+													str = fmt.Sprintf("Verdict => BUY %s %s with %s %s\n\nCurrent Price: $%f\nBuy Limit: $%f\n\nCurrent Price has reached Buy Limit Price and the liquidity ratio is good in the pool\n", s.String(), quoteResDto.ToToken.Symbol, m.String(), quoteResDto.FromToken.Symbol, currentTokenPrice, buyLimit)
+													variables.Verdict = str
+													bot.OutboundChannel <- variables.Verdict
+													if err := router.DoSwap(wallet, stableTokenContractAddress, wallet.StableCoinBalance, targetTokenContractAddress, variables.Mode); err != nil {
+														if err.Error() == "REQUEST_EXPIRED_OR_DECLINED" {
+															err = errors.New("Request expired/declined")
+															bot.OutboundChannel <- err.Error()
+														} else {
+															Die(err)
+														}
+													} else {
+														_, err := rdb.HSet(context.TODO(), strings.ToUpper(variables.StableToken)+"_"+strings.ToUpper(variables.TargetToken), "PreviousTokenPrice", currentTokenPrice).Result()
+														if err != nil {
+															Die(err)
+														} else {
+															_, err := rdb.HSet(context.TODO(), strings.ToUpper(variables.StableToken)+"_"+strings.ToUpper(variables.TargetToken), "SellLimit", currentTokenPrice*float64(1+(variables.ProfitPercent/100))).Result()
+															if err != nil {
+																Die(err)
+															} else {
+																_, err := rdb.HSet(context.TODO(), strings.ToUpper(variables.StableToken)+"_"+strings.ToUpper(variables.TargetToken), "StopLimit", currentTokenPrice*float64(1-(variables.StopLossPercent/100))).Result()
+																if err != nil {
+																	Die(err)
+																}
+															}
+														}
 													}
+												} else {
+													// currentTokenPrice is not a BUY, HODL
+													str = fmt.Sprintf("Verdict => HODL\n\nCurrent Price: $%f\nBuy Limit: $%f\n\nCurrent Price is higher than Buy Limit Price or not enough liquidity in the pool\n", currentTokenPrice, buyLimit)
+													variables.Verdict = str
 												}
 											}
 										}
-									} else {
-										// currentTokenPrice is not a BUY, HODL
-										str = fmt.Sprintf("Verdict => HODL\n\nCurrent Price: $%f\nBuy Limit: $%f\n\nCurrent Price is higher than Buy Limit Price\n", currentTokenPrice, buyLimit)
 									}
 								}
 							}
 						} else if currentStatus == "WAITING_TO_SELL" && wallet.TargetCoinBalance.Cmp(big.NewInt(0)) == 1 {
-
-							v, err := rdb.HGet(context.TODO(), strings.ToUpper(stableToken)+"_"+strings.ToUpper(targetToken), "SellLimit").Result()
+							v, err := rdb.HGet(context.TODO(), strings.ToUpper(variables.StableToken)+"_"+strings.ToUpper(variables.TargetToken), "SellLimit").Result()
 							if err == redis.Nil {
 								e := "No Sell limit is set. Please set a sell limit in the web interface or through the bot."
 								bot.OutboundChannel <- e
@@ -215,7 +198,7 @@ func main() {
 								if err != nil {
 									bot.OutboundChannel <- err.Error()
 								} else {
-									v, err := rdb.HGet(context.TODO(), strings.ToUpper(stableToken)+"_"+strings.ToUpper(targetToken), "StopLimit").Result()
+									v, err := rdb.HGet(context.TODO(), strings.ToUpper(variables.StableToken)+"_"+strings.ToUpper(variables.TargetToken), "StopLimit").Result()
 									if err == redis.Nil {
 										e := "No stop limit is set. Please set a stop limit in the web interface or through the bot."
 										bot.OutboundChannel <- e
@@ -226,7 +209,7 @@ func main() {
 										if err != nil {
 											bot.OutboundChannel <- err.Error()
 										} else {
-											v, err := rdb.HGet(context.TODO(), strings.ToUpper(stableToken)+"_"+strings.ToUpper(targetToken), "PreviousTokenPrice").Result()
+											v, err := rdb.HGet(context.TODO(), strings.ToUpper(variables.StableToken)+"_"+strings.ToUpper(variables.TargetToken), "PreviousTokenPrice").Result()
 											if err == redis.Nil {
 												e := "Could not find previous token price."
 												bot.OutboundChannel <- e
@@ -243,7 +226,9 @@ func main() {
 													if isASell {
 														// Print stats
 														str = fmt.Sprintf("Verdict => SELL (%s: %.2f%s)", typ, value, "%")
-														if err := router.DoSwap(wallet, targetTokenContractAddress, wallet.TargetCoinBalance, stableTokenContractAddress, variables.BotMode); err != nil {
+														variables.Verdict = str
+														bot.OutboundChannel <- variables.Verdict
+														if err := router.DoSwap(wallet, targetTokenContractAddress, wallet.TargetCoinBalance, stableTokenContractAddress, variables.Mode); err != nil {
 															if err.Error() == "REQUEST_EXPIRED_OR_DECLINED" {
 																err = errors.New("Request expired/declined")
 																bot.OutboundChannel <- err.Error()
@@ -251,7 +236,7 @@ func main() {
 																Die(err)
 															}
 														} else {
-															_, err := rdb.HSet(context.TODO(), strings.ToUpper(stableToken)+"_"+strings.ToUpper(targetToken), "BuyLimit", 0).Result()
+															_, err := rdb.HSet(context.TODO(), strings.ToUpper(variables.StableToken)+"_"+strings.ToUpper(variables.TargetToken), "BuyLimit", 0).Result()
 															if err != nil {
 																Die(err)
 															}
@@ -259,6 +244,7 @@ func main() {
 													} else {
 														// currentTokenPrice is not a SELL, HODL
 														str = fmt.Sprintf("Verdict => HODL\n\nCurrent Price: $%f\nSell Limit: $%f\n\nCurrent Price is lower than Sell Limit Price\n", currentTokenPrice, sellLimit)
+														variables.Verdict = str
 													}
 												}
 											}
@@ -267,8 +253,6 @@ func main() {
 								}
 							}
 						}
-						variables.Verdict = str
-						log.Println(variables.Verdict)
 					}
 				}
 			}
@@ -280,7 +264,7 @@ func main() {
 
 func Die(err error) {
 	bot.OutboundChannel <- err.Error()
-	err = errors.New("Bot is now going to die.")
+	err = errors.New("Kryptonite bot is now going to exit. Good bye.")
 	bot.OutboundChannel <- err.Error()
 	time.Sleep(5 * time.Second)
 	os.Exit(1)
--- variables/variables.go
+++ variables/variables.go
@@ -1,8 +1,48 @@
 package variables
 
+import (
+	"errors"
+	"flag"
+)
+
 // Shared variables
 var (
-	BotMode       string
-	CurrentStatus string
-	Verdict       string
+	Address, PrivateKey, StableToken, TargetToken, BotToken, Password, RedisAddress, Mode, ChatID, CurrentStatus, Verdict string
+	ChainID, ProfitPercent, StopLossPercent, Slippage                                                                     uint64
 )
+
+func ParseFlags() error {
+	// Parse input arguments
+	flag.StringVar(&Address, "publicKey", "", "Your wallet public address")
+	flag.StringVar(&PrivateKey, "privateKey", "", "Your wallet private key")
+	flag.StringVar(&Mode, "mode", "MANUAL", "Mode of operation. Allowed options: 'MANUAL' (requires you to authorize a swap via telgram bot), 'AUTO' (does not require any confirmation)")
+	flag.StringVar(&ChatID, "chatId", "", "Your telegram chat id. You will receive this when you authorize yourself with the bot for the first time")
+	flag.StringVar(&StableToken, "stableToken", "USDC", "Stable token (ERC20) to use. Example: USDC, USDT, DAI")
+	flag.StringVar(&TargetToken, "targetToken", "WETH", "Target ERC20 token to hold. Example: WETH, WMATIC, LINK.")
+	flag.StringVar(&BotToken, "botToken", "", "Telegram bot token used to send and receive transaction confirmations")
+	flag.StringVar(&Password, "password", "kryptonite", "Password to share with the bot to authorize yourself as the admin")
+	flag.StringVar(&RedisAddress, "redisAddress", "", "Redis server host. Example: 192.168.1.100:6379")
+	flag.Uint64Var(&ChainID, "chainId", 1, "Chain to use. Allowed options: 1 (Ethereum), 10 (Optimism), 56 (Binance Smart Chain), 137 (Polygon/Matic), 42161 (Arbitrum)")
+	flag.Uint64Var(&ProfitPercent, "profitPercent", 50, "Profit percent at which the bot will execute a sell order")
+	flag.Uint64Var(&StopLossPercent, "stopLossPercent", 25, "Loss percent at which the bot will execute a stop loss order")
+	flag.Uint64Var(&Slippage, "slippagePercent", 1, "Maximum slippage allowed during a swap")
+	flag.Parse()
+
+	if BotToken == "" {
+		return errors.New("botToken is not provided")
+	}
+
+	if PrivateKey == "" {
+		return errors.New("privateKey is not provided")
+	}
+
+	if RedisAddress == "" {
+		return errors.New("redis address is not provided")
+	}
+
+	if (Mode != "MANUAL") && (Mode != "AUTO") {
+		return errors.New("unsupported mode")
+	}
+
+	return nil
+}
