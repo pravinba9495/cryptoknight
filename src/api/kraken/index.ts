fad3132fe54fed054700f2e13e39e0d2c0681160
Praveen Ravichandran <me@pravinba9495.dev>
2022-01-27 23:41:56 -0500

    Handle kraken api error

--- src/api/kraken/index.ts
+++ src/api/kraken/index.ts
@@ -17,6 +17,9 @@ export class Kraken {
     })
       .then((response) => response.data)
       .then((response: any) => {
+        if (response.error.length > 0) {
+          return Promise.reject(response.error[0]);
+        }
         return Number(response["result"][pair.toUpperCase()]["a"][0]);
       })
       .catch((error) => {
