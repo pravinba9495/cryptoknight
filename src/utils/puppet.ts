f94574cf8588876ffcf74b79bfc458d89b2bc00d
Praveen Ravichandran <me@pravinba9495.dev>
2022-02-18 23:13:20 -0500

    Close puppeteer window on error/timeout

--- src/utils/puppet.ts
+++ src/utils/puppet.ts
@@ -1,40 +1,64 @@
 import puppeteer from "puppeteer";
+import { timeout } from "./timeout";
 
-export const GetTradeSignal = async (ticker: string, interval: string) => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    defaultViewport: {
-      width: 1920,
-      height: 1080,
-    },
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.goto(`https://www.tradingview.com/symbols/${ticker}/technicals/`);
-  await page.waitForSelector(`button[id="${interval}"]`);
-  await page.click(`button[id="${interval}"]`);
-  await page.waitForTimeout(5000);
-  const elements = await page.$$(".speedometerSignal-DPgs-R4s");
-  if (elements.length !== 3) {
-    return Promise.reject(
-      "Puppeteer could not fetch trade signals from TradingView"
-    );
-  }
-  const promises: any[] = [];
-  elements.forEach((element, index) => {
-    if (index === 1) {
-      promises.push(
-        page.evaluate((e) => {
-          return e.textContent;
-        }, element)
+export const GetTradeSignal = async (
+  ticker: string,
+  interval: string
+): Promise<string> => {
+  const fn = async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      defaultViewport: {
+        width: 1920,
+        height: 1080,
+      },
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    let isBuy = false;
+    let isSell = false;
+
+    try {
+      const page = await browser.newPage();
+      await page.goto(
+        `https://www.tradingview.com/symbols/${ticker}/technicals/`,
+        {
+          timeout: 10000,
+        }
       );
+      await page.waitForSelector(`button[id="${interval}"]`, {
+        timeout: 10000,
+      });
+      await page.click(`button[id="${interval}"]`);
+      await page.waitForTimeout(10000);
+      const elements = await page.$$(".speedometerSignal-DPgs-R4s");
+      if (elements.length !== 3) {
+        return Promise.reject(
+          "Puppeteer could not fetch trade signals from TradingView"
+        );
+      }
+      const promises: any[] = [];
+      elements.forEach((element, index) => {
+        if (index === 1) {
+          promises.push(
+            page.evaluate((e) => {
+              return e.textContent;
+            }, element)
+          );
+        }
+      });
+      const signals = await Promise.all(promises);
+      isBuy =
+        signals.filter((s) => s.includes("Buy")).length === signals.length;
+      isSell =
+        signals.filter((s) => s.includes("Sell")).length === signals.length;
+      await browser.close();
+    } catch (error) {
+      console.error(error);
+      await browser.close();
+      return Promise.reject(error);
     }
-  });
-  const signals = await Promise.all(promises);
-  const isBuy =
-    signals.filter((s) => s.includes("Buy")).length === signals.length;
-  const isSell =
-    signals.filter((s) => s.includes("Sell")).length === signals.length;
-  await browser.close();
-  return isBuy ? "BUY" : isSell ? "SELL" : "HOLD";
+    return isBuy ? "BUY" : isSell ? "SELL" : "HOLD";
+  };
+  const signal = await timeout(fn(), 60000);
+  return signal;
 };
